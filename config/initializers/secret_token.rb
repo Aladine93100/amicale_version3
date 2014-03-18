1cbe29daa6ccace487fe62fa897bd82fca79d508
Aladine <alazaiane@yahoo.fr>
2014-03-18 12:21:19 +0100

    premier commit avec ajout de la view, fichier JS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmicaleVersion3::Application.config.secret_key_base = '25ed24a44851373bfec2f31c63e0fd12a9fb0a830fe3d2e575c1f05ce1352adce00e244dcab4b4104b36c0a57edb5bb5c7032beae940dadf3e02779d4faaa340'
